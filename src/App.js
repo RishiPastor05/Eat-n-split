ece58383ded3e935cec7fbc3e7cb710be2f7e747
rishipastor05 <rishipastor1607@gmail.com>
2025-11-01 00:03:27 +0530

    Some changes

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ export default function App() {
         {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
 
         <Button onClick={handleShowAddFriend}>
-          {showAddFriend ? "Close" : "Add friend"}
+          {showAddFriend ? "Close" : "Add Friend"}
         </Button>
       </div>
 
